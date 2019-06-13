33eb5ba959138fb8d8a3f9e1746272764b1ff458
Francis Distor <francis.distor@gmail.com>
2019-06-13 14:07:16 -0700

    require info about company

--- src/coverLetter.js
+++ src/coverLetter.js
@@ -1,3 +1,5 @@
-module.exports = (company, position, recruiter, fullName) => {
-  return `Hi ${recruiter},\n\nI'm interested in the ${position} at ${company}. \n\nFrom,\n\n${fullName}`;
+module.exports = (company, position, recruiter, fullName, snippet) => {
+  return `Hi ${recruiter},\n\nI'm interested in the ${position}. What ${company} is doing ${snippet} is great and I want to be a part of it. \n\nFrom,\n\n${fullName}`;
 };
+
+module.exports();
