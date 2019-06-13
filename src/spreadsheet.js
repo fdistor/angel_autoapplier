32dd099e37747e0fae26baf1c5cdc1947c82bab3
Francis Distor <francis.distor@gmail.com>
2019-06-13 01:48:46 -0700

    can query rows based on if not applied and if link to job exists

---
+++ src/spreadsheet.js
@@ -0,0 +1,20 @@
+const GoogleSpreadsheet = require("google-spreadsheet");
+const { promisify } = require("util");
+const { sheetsId } = require("../config/config.js");
+
+const creds = require("../keys/client_secret.json");
+
+const accessSpreadsheet = async () => {
+  const doc = new GoogleSpreadsheet(sheetsId);
+  await promisify(doc.useServiceAccountAuth)(creds);
+  const info = await promisify(doc.getInfo)();
+  const sheet = info.worksheets[0];
+
+  const rows = await promisify(sheet.getRows)({
+    offset: 1,
+    limit: 91,
+    query: "applied = No and haslink = TRUE"
+  });
+};
+
+accessSpreadsheet();
