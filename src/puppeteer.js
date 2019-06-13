a998a602c9fe8b7e6167a2e927aaf5f0fb7b8c0c
Francis Distor <francis.distor@gmail.com>
2019-06-13 02:03:00 -0700

    log in user successful

--- src/puppeteer.js
+++ src/puppeteer.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+const { user, password } = require("../config/config.js");
 
 const autoApply = async jobs => {
   const browser = await puppeteer.launch({
@@ -9,10 +10,20 @@ const autoApply = async jobs => {
   const homepage = "https://angel.co";
   const page = await browser.newPage();
 
-  // Log in user
+  // Navigate to log in page
   await page.goto(homepage);
   await page.waitForSelector(".auth.login");
   await page.click(".auth.login");
+
+  // Log in user
+  await page.waitForSelector("#user_email");
+  await page.click("#user_email");
+  await page.type("#user_email", user);
+  await page.waitForSelector("#user_password");
+  await page.click("#user_password");
+  await page.type("#user_password", password);
+  await page.click(".c-button");
+
   await page.waitFor(3000);
 
   // Close Browser once finished
