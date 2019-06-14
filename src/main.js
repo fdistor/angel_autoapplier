944bdeace18df201530987ce4ef17c8a8867f0b9
Francis Distor <francis.distor@gmail.com>
2019-06-14 12:14:32 -0700

    initialize main function

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 const sheets = require("./spreadsheet.js");
 const auto = require("./puppeteer");
 const clearbit = require("./clearbit");
+
+const applyToAngel = async () => {
+  const jobs = await sheets.accessSpreadsheet();
+};
+
+applyToAngel();
