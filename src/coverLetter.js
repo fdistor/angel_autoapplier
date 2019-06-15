3640cf2a2b24a3a1498bb0de266854f71705870f
Francis Distor <francis.distor@gmail.com>
2019-06-15 13:45:26 -0700

    add enthusiasm

--- src/coverLetter.js
+++ src/coverLetter.js
@@ -1,4 +1,4 @@
 module.exports = (company, position, recruiter, fullName, snippet) => {
-  const coverLetter = `Hi ${recruiter},\n\nI'm interested in the ${position} role. What ${company} is doing ${snippet} is great and I want to be a part of it. \n\nFrom,\n\n${fullName}`;
+  const coverLetter = `Hi ${recruiter},\n\nI'm interested in the ${position} role. What ${company} is doing ${snippet} is great and I want to be a part of it! \n\nFrom,\n\n${fullName}`;
   return coverLetter;
 };
