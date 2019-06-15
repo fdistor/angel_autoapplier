66d10fdb0976b1b4ee312a35c65984dda24935ba
Francis Distor <francis.distor@gmail.com>
2019-06-15 12:17:52 -0700

    give cover letter own variable

--- src/coverLetter.js
+++ src/coverLetter.js
@@ -1,3 +1,4 @@
 module.exports = (company, position, recruiter, fullName, snippet) => {
-  return `Hi ${recruiter},\n\nI'm interested in the ${position}. What ${company} is doing ${snippet} is great and I want to be a part of it. \n\nFrom,\n\n${fullName}`;
+  const coverLetter = `Hi ${recruiter},\n\nI'm interested in the ${position}. What ${company} is doing ${snippet} is great and I want to be a part of it. \n\nFrom,\n\n${fullName}`;
+  return coverLetter;
 };
