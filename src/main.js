100c94f92cf3f40584091538b2ae0dbd127fd8a2
fdistor <francis.distor@gmail.com>
2019-08-20 23:12:07 -0700

    pass in query to accessSpreadsheet

--- src/main.js
+++ src/main.js
@@ -2,18 +2,20 @@ const sheets = require('./spreadsheet.js');
 const auto = require('./puppeteer');
 const SearchRecruiterEmail = require('./clearbit');
 
+const applyToJobsQuery = 'applied = No and haslink = TRUE';
+
 const applyToAngel = async () => {
-  const jobs = await sheets.accessSpreadsheet();
-  const updatedJobs = await auto.autoApply(jobs);
+	const jobs = await sheets.accessSpreadsheet(applyToJobsQuery);
+	const updatedJobs = await auto.autoApply(jobs);
 
-  for (let job of updatedJobs) {
-    const { domain, recruiter } = job;
-    const searchRecruiterEmail = new SearchRecruiterEmail(domain, recruiter);
+	for (let job of updatedJobs) {
+		const { domain, recruiter } = job;
+		const searchRecruiterEmail = new SearchRecruiterEmail(domain, recruiter);
 
-    job.email = await searchRecruiterEmail.searchByDomain();
+		job.email = await searchRecruiterEmail.searchByDomain();
 
-    sheets.updateSpreadsheetRow(job);
-  }
+		sheets.updateSpreadsheetRow(job);
+	}
 };
 
 applyToAngel();
