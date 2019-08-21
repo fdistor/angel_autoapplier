1a0d89a222bdaa4f676f229315b0a7e097979899
fdistor <francis.distor@gmail.com>
2019-08-20 23:10:12 -0700

    reformat

--- src/spreadsheet.js
+++ src/spreadsheet.js
@@ -5,22 +5,22 @@ const { sheetsId } = require('../config/config.js');
 const creds = require('../keys/client_secret.json');
 
 module.exports = {
-  accessSpreadsheet: async () => {
-    const doc = new GoogleSpreadsheet(sheetsId);
-    await promisify(doc.useServiceAccountAuth)(creds);
-    const info = await promisify(doc.getInfo)();
-    const sheet = info.worksheets[0];
+	accessSpreadsheet: async () => {
+		const doc = new GoogleSpreadsheet(sheetsId);
+		await promisify(doc.useServiceAccountAuth)(creds);
+		const info = await promisify(doc.getInfo)();
+		const sheet = info.worksheets[0];
 
-    const rows = await promisify(sheet.getRows)({
-      offset: 1,
-      limit: 120,
-      query: 'applied = No and haslink = TRUE'
-    });
+		const rows = await promisify(sheet.getRows)({
+			offset: 1,
+			limit: 120,
+			query: 'applied = No and haslink = TRUE'
+		});
 
-    return rows;
-  },
+		return rows;
+	},
 
-  updateSpreadsheetRow: async job => {
-    await job.save();
-  }
+	updateSpreadsheetRow: async job => {
+		await job.save();
+	}
 };
