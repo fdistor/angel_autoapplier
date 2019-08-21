f5f81397e16d367511be0c634f7b1bfbce069112
fdistor <francis.distor@gmail.com>
2019-08-21 00:18:00 -0700

    remove old code

--- src/followUp.js
+++ src/followUp.js
@@ -1,16 +1,3 @@
-const { gmailUser, gmailPassword, myFullName } = require('../config/config.js');
-const options = {
-	user: gmailUser,
-	pass: gmailPassword,
-	to: 'francis.distor@gmail.com',
-	subject: 'test'
-};
-const send = require('gmail-send')(options);
-
-// send({ text: 'test' })
-// 	.then(result => console.log(result))
-// 	.catch(err => console.error('Error:', err));
-
 class FollowUp {
 	constructor(jobs) {
 		this.jobs = jobs;
@@ -81,7 +68,7 @@ ${myFullName}
 
 	followUpAllJobs(jobs) {
     jobs.forEach(job => await this.followUpOneJob(job));
-    
+
     return jobs;
 	}
 }
