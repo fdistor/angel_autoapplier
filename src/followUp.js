87e8d71c6d1c770f80b372a8b7794b498afdcfbe
fdistor <francis.distor@gmail.com>
2019-08-20 23:59:47 -0700

    add function to get month string from the month digit

--- src/followUp.js
+++ src/followUp.js
@@ -39,6 +39,25 @@ ${myFullName}
 `;
 	}
 
+	getMonth(monthDigit) {
+		const months = {
+			1: 'January',
+			2: 'February',
+			3: 'March',
+			4: 'April',
+			5: 'May',
+			6: 'June',
+			7: 'July',
+			8: 'August',
+			9: 'September',
+			10: 'October',
+			11: 'November',
+			12: 'December'
+		};
+
+		return months[monthDigit];
+	}
+
 	getFirstName(fullName) {
 		return fullName.split(' ')[0];
 	}
